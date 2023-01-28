cbef6aa3659453e33a2f5927ec84360794f79842
Kirill Allyanov <kir.allyanov@gmail.com>
2023-01-28 20:04:55 +0300

    first commit

---
+++ task1.java
@@ -0,0 +1,19 @@
+package DZ2;
+
+public class task1 {
+    /**
+     * Checks if given <code>text</code> is a palindrome.
+     *
+     * @param text any string
+     * @return <code>true</code> when <code>text</code> is a palindrome, <code>false</code> otherwise
+     */
+    public static boolean isPalindrome(String text) {
+        String replaced = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String reversed = new StringBuffer(replaced).reverse().toString();
+        return reversed.equals(replaced);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam, I'm Adam!"));
+    }
+}
